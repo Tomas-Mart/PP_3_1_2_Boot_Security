1e03bc758323e776cf149461c613ca65147b5bfb
Tomas-Mart <amina.tomas.mart@icloud.com>
2025-03-27 05:00:47 +0400

    Доработка

--- src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImplDetails.java
+++ src/main/java/ru/kata/spring/boot_security/demo/services/UserServiceImplDetails.java
@@ -3,12 +3,8 @@ package ru.kata.spring.boot_security.demo.services;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
-import ru.kata.spring.boot_security.demo.models.User;
 import ru.kata.spring.boot_security.demo.repositories.UserRepository;
-
-import java.util.stream.Collectors;
 
 @Service
 public class UserServiceImplDetails implements UserDetailsService {
@@ -21,15 +17,7 @@ public class UserServiceImplDetails implements UserDetailsService {
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        User user = userRepository.findByUsername(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Пользователь с username " + username + " не найден"));
-
-        return new org.springframework.security.core.userdetails.User(
-                user.getUsername(),
-                user.getPassword(),
-                user.getRoles().stream()
-                        .map(role -> new SimpleGrantedAuthority(role.getName()))
-                        .collect(Collectors.toList())
-        );
+        return userRepository.findByUsername(username).orElseThrow(() ->
+                new UsernameNotFoundException("Пользователь с username " + username + " не найден"));
     }
 }
